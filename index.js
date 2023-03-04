e3a0f90253eab42265e986d4aaf905e9f9273f75
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-03-04 17:15:07 +0500

    become better

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-import fetch from "node-fetch";
 import { Telegraf } from "telegraf"
+import { config } from "dotenv";
+import wiki from "./inlineQuery/wiki.js";
+import pixeby from "./inlineQuery/pix.js";
+
+config()
 const TOKEN = '6192276647:AAEBK5_8KSJmlMw9Zr9xA-ewLwnwB0_4pSU';
 
 const bot = new Telegraf(TOKEN);
 
-bot.use(ctx => {
-    console.log(ctx.chat)
-})
-
-const data = {
-    chat_id: "-1001600920538",
-    text: "Ishlamdi"
-};
-
-(async () => {
-    const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`,
-    {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-})();
-
+pixeby(bot)
+wiki(bot)
 
 bot.launch()
